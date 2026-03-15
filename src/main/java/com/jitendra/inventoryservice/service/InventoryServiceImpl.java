cf5a733a7f59e7a09090cde8dfadb1f433234e45
Jitendra Kumar <kjitendra9080@gmail.com>
2026-03-15 13:09:41 +0530

    distributed transaction implemented using saga pattren using kafka message queue

--- src/main/java/com/jitendra/inventoryservice/service/InventoryServiceImpl.java
+++ src/main/java/com/jitendra/inventoryservice/service/InventoryServiceImpl.java
@@ -1,11 +1,19 @@
 package com.jitendra.inventoryservice.service;
 
+
+
+import com.jitendra.event.InventoryFailedEvent;
+import com.jitendra.event.InventoryReservedEvent;
+import com.jitendra.event.OrderCreatedEvent;
 import com.jitendra.inventoryservice.exception.ResourceNotFoundException;
 import com.jitendra.inventoryservice.model.Inventory;
 import com.jitendra.inventoryservice.repository.InventoryRepository;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
 
 
 @Service
@@ -13,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class InventoryServiceImpl implements InventoryService {
 
     private final InventoryRepository inventoryRepository;
-
+    private final KafkaTemplate<String,Object> kafkaTemplate;
     @Override
     public Inventory getInventory(Long productId) {
 
@@ -64,4 +72,41 @@ public class InventoryServiceImpl implements InventoryService {
 
         inventoryRepository.save(inventory);
     }
+    public Boolean checkStock(Long productId, Integer quantity) {
+       Optional<Inventory> inventory = inventoryRepository.findByProductId(productId);
+       if(inventory.isPresent()) {
+           if (inventory.get().getAvailableQuantity() >=0) return true;
+       }
+        return true;
+    }
+
+    @KafkaListener(topics = "order-created", groupId = "inventory-group")
+    public void consumeOrderCreated(OrderCreatedEvent event) {
+
+        boolean available = checkStock(event.getProductId(), event.getQuantity());
+        InventoryReservedEvent event1=new InventoryReservedEvent();
+
+        if (available) {
+            InventoryReservedEvent event2=new InventoryReservedEvent();
+            event2.setOrderId(event.getOrderId());
+            event2.setProductId(event.getProductId());
+            kafkaTemplate.send("inventory-reserved",event2 );
+        } else {
+            InventoryFailedEvent  event3=new InventoryFailedEvent();
+            event3.setOrderId(event.getOrderId());
+            event3.setProductId(event.getProductId());
+            kafkaTemplate.send("inventory-failed",event3);
+        }
+    }
+//    @KafkaListener(topics = "payment-success")
+//    public void reserveInventory(PaymentSuccessEvent event){
+//
+//        System.out.println("Reserving inventory");
+//
+//        InventoryReservedEvent inventoryEvent =
+//                new InventoryReservedEvent(event.getOrderId(),"P100");
+//
+//        kafkaTemplate.send("inventory-reserved", inventoryEvent);
+//
+//    }
 }
